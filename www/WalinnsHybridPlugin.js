155cdc71442ebc20c9021362bc3863199658b3d6
Walinns-Manivasagam <43805862+Walinns-Manivasagam@users.noreply.github.com>
2018-10-04 12:11:02 +0530

    Update code

--- www/WalinnsHybridPlugin.js
+++ www/WalinnsHybridPlugin.js
@@ -1,5 +1,16 @@
+cordova.define("com.plugin.walinnshybrid", function(require, exports, module) {
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'WalinnsHybridPlugin', 'coolMethod', [arg0]);
 };
+
+exports.trackEvent = function (arg0, success, error) {
+    exec(success, error, 'WalinnsHybridPlugin', 'trackEvent', [arg0]);
+};
+
+exports.trackScreen = function (arg0, success, error) {
+    exec(success, error, 'WalinnsHybridPlugin', 'trackScreen', [arg0]);
+};
+
+});
