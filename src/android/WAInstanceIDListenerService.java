94610b3241f99d699627634278d9a3da4d360479
Walinns-Manivasagam <43805862+Walinns-Manivasagam@users.noreply.github.com>
2018-10-04 12:52:45 +0530

    Added sdk files

---
+++ src/android/WAInstanceIDListenerService.java
@@ -0,0 +1,19 @@
+package com.plugin.walinnshybrid;
+
+import android.content.Intent;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+/**
+ * Created by walinnsinnovation on 08/05/18.
+ */
+
+public class WAInstanceIDListenerService extends InstanceIDListenerService {
+    private static final String TAG = "MyInstanceIDLS";
+    @Override
+    public void onTokenRefresh() {
+        // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
+        Intent intent = new Intent(this, WAIntentService.class);
+        startService(intent);
+    }
+}
